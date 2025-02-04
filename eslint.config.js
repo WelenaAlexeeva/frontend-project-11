eaec659c9dd270585346ffa010743f8e3aa87aa4
Welena <welena@list.ru>
2025-02-04 14:56:54 +0700

    change eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    ignorePatterns: ["dist/"],
+  },
   pluginJs.configs.recommended,
 ];
